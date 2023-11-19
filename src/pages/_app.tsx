2f20087f0e70482e370254eae22a1b90e384787b
gammahazard <dhaliswag777@gmail.com>
2023-11-19 02:34:11 -0500

    ded

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,22 @@
-// src/pages/_app.tsx
-
 import '../styles/globals.css';
 import Theme from '../components/Theme';
 import CustomCursor from '../components/CustomCursor';
 import type { AppProps } from 'next/app';
-import MaintenanceMode from '../components/MaintenanceMode';  // Import the new component
+import MaintenanceMode from '../components/MaintenanceMode'; // Import the new component
+import Head from 'next/head'; // Import Head from next/head
 
 const MAINTENANCE_MODE = false; // Set to false to turn off maintenance mode
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Theme>
+      <Head>
+        <title>DePunkz</title> {/* Optional: Update the title */}
+        <link rel="icon" href="/favicon.png" /> {/* Add the favicon link here */}
+      </Head>
       <CustomCursor />
       <Component {...pageProps} />
-      <MaintenanceMode active={MAINTENANCE_MODE} />  
+      <MaintenanceMode active={MAINTENANCE_MODE} />
     </Theme>
   );
 }
